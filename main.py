df1d55b38f7fa9a3c70a22d374fbd723bdc86136
Emily Son <sonemily9+github@gmail.com>
2022-05-01 14:46:35 -0400

    Add date format validation loop.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import datetime
 from billboard_hot_100_data import TimeMachine
 
-date = input("Which year do you want to travel to? Type in this format: YYYY-MM-DD: ")
 
-data = TimeMachine(date)
-print(data.top_100_songs)
+def validate(date):
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        return False
+
+
+date = input("Which year do you want to travel to? Type in this format: YYYY-MM-DD: ")
+while validate(date) == False:
+    date = input("Which year do you want to travel to? Type in this format: YYYY-MM-DD: ")
+song_data = TimeMachine(date)
+print(song_data.top_100_songs)
