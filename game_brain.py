bc7891111ecaf7cbeaec96d895f08f050d3542e6
Emily Son <sonemily9+github@gmail.com>
2022-05-01 15:48:06 -0400

    Add loop_check that adds an extra step to check and see if the game continues.

--- game_brain.py
+++ game_brain.py
@@ -1,16 +1,31 @@
 import random
 songs_chosen = []
 
+
 class GameBrain:
 
     def __init__(self, song_data):
         self.song_data = song_data
         self.song2 = random.choice(self.song_data)
+        self.song1 = None
+        self.answer = None
         self.score = 0
         self.game_continue = True
 
-        #Might have to separate this chunk elsewhere
-        while self.game_continue:
+        # #Might have to separate this chunk elsewhere
+        # while self.game_continue:
+        #     self.song1 = self.song2
+        #     self.song2 = random.choice(self.song_data)
+        #     while self.song1 == self.song2 or self.song2 in songs_chosen:
+        #         self.song2 = random.choice(self.song_data)
+        #
+        #     songs_chosen.append(self.song2)
+        #
+        #     # This just closes the loop so I can keep testing
+        #     self.game_continue = False
+
+    def loop_check(self, continue_check):
+        while continue_check:
             self.song1 = self.song2
             self.song2 = random.choice(self.song_data)
             while self.song1 == self.song2 or self.song2 in songs_chosen:
@@ -19,4 +34,4 @@ class GameBrain:
             songs_chosen.append(self.song2)
 
             # This just closes the loop so I can keep testing
-            self.game_continue = False
+            # continue_check = False
