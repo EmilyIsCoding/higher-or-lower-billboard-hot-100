0cf674f6ed84d0a9ee39c90c219ba5f3e9f2abaa
Emily Son <sonemily9+github@gmail.com>
2022-05-01 15:50:13 -0400

    Change last commit loop_check to next_song, so that pressing the next button in the UI manually controls the loop.

--- game_brain.py
+++ game_brain.py
@@ -24,14 +24,14 @@ class GameBrain:
         #     # This just closes the loop so I can keep testing
         #     self.game_continue = False
 
-    def loop_check(self, continue_check):
-        while continue_check:
-            self.song1 = self.song2
+    def next_song(self):
+        self.song1 = self.song2
+        self.song2 = random.choice(self.song_data)
+        while self.song1 == self.song2 or self.song2 in songs_chosen:
             self.song2 = random.choice(self.song_data)
-            while self.song1 == self.song2 or self.song2 in songs_chosen:
-                self.song2 = random.choice(self.song_data)
 
-            songs_chosen.append(self.song2)
+        songs_chosen.append(self.song2)
 
-            # This just closes the loop so I can keep testing
-            # continue_check = False
+        # This just closes the loop so I can keep testing
+        # continue_check = False
+
