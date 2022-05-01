f060eb9783719c5718d95ae734d21151374dba93
Emily Son <sonemily9+github@gmail.com>
2022-05-01 15:34:21 -0400

    Update GameBrain with songs_chosen list to make sure only new songs are used for song2.

--- game_brain.py
+++ game_brain.py
@@ -1,18 +1,22 @@
 import random
-
+songs_chosen = []
 
 class GameBrain:
 
     def __init__(self, song_data):
         self.song_data = song_data
         self.song2 = random.choice(self.song_data)
+        self.score = 0
         self.game_continue = True
+
+        #Might have to separate this chunk elsewhere
         while self.game_continue:
             self.song1 = self.song2
             self.song2 = random.choice(self.song_data)
-
-            while self.song1 == self.song2:
+            while self.song1 == self.song2 or self.song2 in songs_chosen:
                 self.song2 = random.choice(self.song_data)
+
+            songs_chosen.append(self.song2)
 
             # This just closes the loop so I can keep testing
             self.game_continue = False
